30069ff90c10f00a4f08e865475df8f7f20329d0
Omar Salka <omarsalka9@gmail.com>
2019-09-16 17:39:57 -0500

    added models and middleware

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware - to accept data/body data
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the Tech Recruiter API...' })
 );
