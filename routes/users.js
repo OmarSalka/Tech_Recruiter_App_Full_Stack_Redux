b8463cf79c0ee77ba5a018d4a113492da19f08c9
Omar Salka <omarsalka9@gmail.com>
2019-09-17 08:31:50 -0500

    generates login token

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const { check, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -48,7 +49,24 @@ router.post(
 
       await user.save();
 
-      res.send('UserSaved');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      // to generate a token we have to sign it
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000 // remember to change it to 3600 before deployment
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send('Server Error');
