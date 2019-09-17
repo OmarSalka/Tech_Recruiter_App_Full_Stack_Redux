72a1e332e2549bd950e890b5a7d454df6ef48d8c
Omar Salka <omarsalka9@gmail.com>
2019-09-17 13:06:46 -0500

    authentication route complete

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+// middleware is a function that has access to the request and response cycle and object
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+  // Get Token from header
+  const token = req.header('x-auth-token');
+
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, auhorization denied' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'token is not valid' });
+  }
+};
