46b4dd0076454caa8da4dc22b71f083123ede2a3
Omar Salka <omarsalka9@gmail.com>
2019-10-30 11:35:40 -0500

    prepare to deploy

--- server.js
+++ server.js
@@ -17,14 +17,15 @@ app.use('/api/candidate', require('./routes/candidate'));
 app.use('/api/filter', require('./routes/filter'));
 
 //Serve static assets if in production
-// if (process.env.NODE_ENV === 'production') {
-// Set static folder
-app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname + '/client/build')));
 
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-);
-// }
+  app.get('*', (req, res) => {
+    // res.sendFile(path.resolve(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
