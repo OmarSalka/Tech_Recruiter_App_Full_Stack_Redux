f5ceff4156668587eab9a8beba30c607bac91f39
Omar Salka <omarsalka9@gmail.com>
2019-10-30 20:34:33 -0500

    again

---
+++ client/build/precache-manifest.3b06db66ec2526987cc9a974a33e443c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30fbe62aa204ef07be911252e285a1b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1acc413c0e9473d208a6",
+    "url": "/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "93dd79dcb79b7d95a155",
+    "url": "/static/css/main.1914c17f.chunk.css"
+  },
+  {
+    "revision": "1acc413c0e9473d208a6",
+    "url": "/static/js/2.506a9a57.chunk.js"
+  },
+  {
+    "revision": "93dd79dcb79b7d95a155",
+    "url": "/static/js/main.03478263.chunk.js"
+  },
+  {
+    "revision": "e98899a1e8647211ef64",
+    "url": "/static/js/runtime~main.89771464.js"
+  },
+  {
+    "revision": "933bd491a7d6cdd3b23deff876593324",
+    "url": "/static/media/unsplash3_3_2.933bd491.jpg"
+  }
+]);
