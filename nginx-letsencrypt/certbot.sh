a28da71de8d4bbcccbfe392b8611dac20ae2cb24
Omar Salka <omarsalka@Omars-MBP.attlocal.net>
2020-01-04 17:18:04 -0600

    implementing new ssl configuration

--- nginx-letsencrypt/certbot.sh
+++ nginx-letsencrypt/certbot.sh
@@ -4,15 +4,20 @@ fi
 certbot certonly \
         --config-dir "${LETSENCRYPT_DIR:-/etc/letsencrypt}" \
 		--agree-tos \
-		--domains "$DOMAIN" \
-		--email "$EMAIL" \
+		# --domains "$DOMAIN" \
+		--domains "www.techrecruiterapp.com" \
+		# --email "$EMAIL" \
+		--email "omarsalka9@gmail.com" \
 		--expand \
 		--noninteractive \
 		--webroot \
 		--webroot-path /var/www/certbot \
 		$OPTIONS || true
 
-if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem" ]]; then
-    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem" /usr/share/nginx/certificates/privkey.pem
-    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
+# if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem" ]]; then
+#     cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem" /usr/share/nginx/certificates/privkey.pem
+#     cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
+if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/www.techrecruiterapp.com/privkey.pem" ]]; then
+    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/www.techrecruiterapp.com/privkey.pem" /usr/share/nginx/certificates/privkey.pem
+    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/www.techrecruiterapp.com/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
 fi
