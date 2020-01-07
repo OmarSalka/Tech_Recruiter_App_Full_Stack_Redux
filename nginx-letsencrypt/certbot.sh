395ad8bc91ebecbdc19641448ac533d4df5405bd
Omar Salka <omarsalka@Omars-MBP.attlocal.net>
2020-01-07 16:12:12 -0600

    resolving errors

--- nginx-letsencrypt/certbot.sh
+++ nginx-letsencrypt/certbot.sh
@@ -1,18 +1,38 @@
+# if [[ ! -f /var/www/techrecruiterapp.com ]]; then
+#     mkdir -p /var/www/techrecruiterapp.com
+# fi
+# certbot certonly \
+#         --config-dir "${LETSENCRYPT_DIR:-/etc/letsencrypt}" \
+# 		--agree-tos \
+# 		--domains "www.techrecruiterapp.com,techrecruiterapp.com" \
+# 		--email "omariosalkos@gmail.com" \
+# 		--expand \
+# 		--noninteractive \
+# 		--webroot \
+# 		--webroot-path /var/www/techrecruiterapp.com \
+# 		--dry-run \
+# 		$OPTIONS || true
+
+# if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/privkey.pem" ]]; then
+#     cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/privkey.pem" /usr/share/nginx/certificates/privkey.pem
+#     cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
+# fi
 if [[ ! -f /var/www/certbot ]]; then
     mkdir -p /var/www/certbot
 fi
 certbot certonly \
         --config-dir "${LETSENCRYPT_DIR:-/etc/letsencrypt}" \
 		--agree-tos \
-		--domains "$DOMAIN" \
-		--email "$EMAIL" \
+		--domains "www.techrecruiterapp.com,techrecruiterapp.com" \
+		--email "omariosalkos@gmail.com" \
 		--expand \
 		--noninteractive \
 		--webroot \
 		--webroot-path /var/www/certbot \
+		--dry-run \
 		$OPTIONS || true
 
-if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem" ]]; then
-    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem" /usr/share/nginx/certificates/privkey.pem
-    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
+if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/privkey.pem" ]]; then
+    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/privkey.pem" /usr/share/nginx/certificates/privkey.pem
+    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
 fi
