b7e550ed2f2806ac263a4994a25b9659f4cca8dd
Omar Salka <omarsalka@Omars-MBP.attlocal.net>
2020-01-07 22:06:02 -0600

    testing new changes

--- nginx-letsencrypt/certbot.sh
+++ nginx-letsencrypt/certbot.sh
@@ -1,5 +1,26 @@
-# if [[ ! -f /var/www/techrecruiterapp.com ]]; then
-#     mkdir -p /var/www/techrecruiterapp.com
+if [[ ! -f /var/www/certbot ]]; then
+    mkdir -p /var/www/certbot
+fi
+certbot certonly \
+		--debug \
+        --config-dir "${LETSENCRYPT_DIR:-/etc/letsencrypt}" \
+		--agree-tos \
+		--domains "www.techrecruiterapp.com" \
+		--domains "techrecruiterapp.com" \
+		--email "omariosalkos@gmail.com" \
+		--expand \
+		--noninteractive \
+		--debug-challenges \
+		--webroot -w /var/www/certbot \
+		--dry-run \
+		$OPTIONS || true
+
+if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/privkey.pem" ]]; then
+    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/privkey.pem" /usr/share/nginx/certificates/privkey.pem
+    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
+fi
+# if [[ ! -f /var/www/certbot ]]; then
+#     mkdir -p /var/www/certbot
 # fi
 # certbot certonly \
 #         --config-dir "${LETSENCRYPT_DIR:-/etc/letsencrypt}" \
@@ -9,7 +30,7 @@
 # 		--expand \
 # 		--noninteractive \
 # 		--webroot \
-# 		--webroot-path /var/www/techrecruiterapp.com \
+# 		--webroot-path /var/www/certbot \
 # 		--dry-run \
 # 		$OPTIONS || true
 
@@ -17,22 +38,3 @@
 #     cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/privkey.pem" /usr/share/nginx/certificates/privkey.pem
 #     cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
 # fi
-if [[ ! -f /var/www/certbot ]]; then
-    mkdir -p /var/www/certbot
-fi
-certbot certonly \
-        --config-dir "${LETSENCRYPT_DIR:-/etc/letsencrypt}" \
-		--agree-tos \
-		--domains "www.techrecruiterapp.com,techrecruiterapp.com" \
-		--email "omariosalkos@gmail.com" \
-		--expand \
-		--noninteractive \
-		--webroot \
-		--webroot-path /var/www/certbot \
-		--dry-run \
-		$OPTIONS || true
-
-if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/privkey.pem" ]]; then
-    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/privkey.pem" /usr/share/nginx/certificates/privkey.pem
-    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/techrecruiterapp.com/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
-fi
